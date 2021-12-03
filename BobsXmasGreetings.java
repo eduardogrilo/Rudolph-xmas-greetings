6f1f6b3750940f7ee7b65e3c4a5cdbe9e5a0afdd
eduardogrilo <jose_egrl@hotmail.com>
2021-12-03 23:00:02 +0000

    Co-authored-by: Luís Capa <LFCC98@users.noreply.github.com>

--- BobsXmasGreetings.java
+++ BobsXmasGreetings.java
@@ -1,24 +1,39 @@
 public class BobsXmasGreetings {
+
+    public static boolean isVowel(char c) {
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'|| c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    }
+
     public static String reverseVowels(String greeting) {
-        char[] vowelsArray = {};
-        int countNumVowels=0;
-        for (int i = greeting.length()-1; i>=0; i++) {
-            char greetingChar = greeting.charAt(i);//Converts every single String character (greeting) into a char
+        int countNumVowels = 0;
+        char[] vowelsArray = new char[greeting.length()];
+        for (int i = greeting.length()-1; i>=0; i--) {
+            char greetingChar1 = greeting.charAt(i); //Converts every single String character (greeting) into a single char
             
-            if (greetingChar == 'a' || greetingChar == 'e' || greetingChar == 'i' || greetingChar == 'o' || greetingChar == 'u'|| greetingChar == 'A' || greetingChar == 'E' || greetingChar == 'I' || greetingChar == 'O' || greetingChar == 'U'){
-                vowelsArray[countNumVowels] = greetingChar;
+            if (isVowel(greetingChar1)) {
+                vowelsArray[countNumVowels] = greetingChar1;
                 countNumVowels++;
             }
-
-
-
         }
-        return "nothing here";
+        
+        int vowelsIndex = 0;
+        char[] greetingCharArray = greeting.toCharArray();//Converts the string (greeting) to a new character array
+        for (int i = 0; i < greeting.length(); i++) {
+            char greetingChar2 = greeting.charAt(i);//Converts every single String character (greeting) into a single char
+            
+            if (isVowel(greetingChar2)){
+                greetingCharArray[i] = vowelsArray[vowelsIndex];
+                vowelsIndex++;
+            }
+        }
+        return new String(greetingCharArray);
     }
 
     public static void main(String[] args) {
-        String test1 = "Eusoumaluco";
+        String test1 = "Eu sou maluco";
         System.out.println(reverseVowels(test1));
+        String test2 = "Aeiou";
+        System.out.println(reverseVowels(test2));
     
     }
 }
