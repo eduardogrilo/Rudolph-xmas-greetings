f91dabff9594b7e5c3f6250a591f8f829259ac90
eduardogrilo <jose_egrl@hotmail.com>
2021-12-03 14:54:17 +0000

    First Step already done (Identifying each vowel and put it in an order)

--- BobsXmasGreetings.java
+++ BobsXmasGreetings.java
@@ -1,5 +1,24 @@
 public class BobsXmasGreetings {
-    public String reverseVowels(String greeting) {
+    public static String reverseVowels(String greeting) {
+        char[] vowelsArray = {};
+        int countNumVowels=0;
+        for (int i = greeting.length()-1; i>=0; i++) {
+            char greetingChar = greeting.charAt(i);//Converts every single String character (greeting) into a char
+            
+            if (greetingChar == 'a' || greetingChar == 'e' || greetingChar == 'i' || greetingChar == 'o' || greetingChar == 'u'|| greetingChar == 'A' || greetingChar == 'E' || greetingChar == 'I' || greetingChar == 'O' || greetingChar == 'U'){
+                vowelsArray[countNumVowels] = greetingChar;
+                countNumVowels++;
+            }
+
+
+
+        }
         return "nothing here";
+    }
+
+    public static void main(String[] args) {
+        String test1 = "Eu sou maluco";
+        System.out.println(reverseVowels(test1));
+    
     }
 }
